2dfa538adfff0893577dd6b566e8c1c246b896a4
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2020-08-27 17:50:46 +0530

    Create pos-neg-sort.py

---
+++ pos-neg-sort.py
@@ -0,0 +1,28 @@
+# Sort positives, keep negatives
+# Write a function that sorts the positive numbers
+# in ascending order and keep the negative numbers untouched
+# Notes: 
+# 1. If given empty list, you should return an empty list
+# 2. Integers will be always positive or negative 0 is not included
+# in the tests.
+#
+def pos_neg_sort(mylist):    
+    sorted_list = mylist.copy()
+    sorted_list.sort()
+    result = filter(lambda x: x > 0, sorted_list) 
+    new_list = list(result).copy()
+
+
+    output = []
+    for idx, val in enumerate(mylist, start=0):
+        if val > 0 and new_list:
+            output.append(new_list.pop(0))
+        else:
+            output.append(val)
+    print("original list ")
+    print(mylist)
+    print("positive num sorted list")
+    print(output)
+
+mylist = [8, 1, -4, -6, 9, 3, -9]
+pos_neg_sort(mylist)
